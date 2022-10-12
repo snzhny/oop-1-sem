06d5f4d870350332eab9a24572c6523344e7821b
hineko <88938702+snzhny@users.noreply.github.com>
2022-10-12 03:39:43 +0300

    Delete laba_5 directory

--- laba_5/CricketBall1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace laba_5;
-
-partial class CricketBall : Ball
-{
-    public CricketBall(string inventory_Name) { inventory_name = inventory_Name; }
-    public void HelloWorld() => Console.WriteLine("Hello World");
-}
